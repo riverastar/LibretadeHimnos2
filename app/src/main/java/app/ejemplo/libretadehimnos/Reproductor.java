41d9673449d466d41c4e583dbfa01c3a6c02848e
riverastar <osvaldor30@outlook.com>
2020-05-02 19:24:16 -0500

    reproductor_full

--- app/src/main/java/app/ejemplo/libretadehimnos/Reproductor.java
+++ app/src/main/java/app/ejemplo/libretadehimnos/Reproductor.java
@@ -19,19 +19,19 @@ import java.io.IOException;
 
 public class Reproductor extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
     private TextView tex1, tex2, tex3;
-    private Button b1, b2, b3, b4, b5,tod;
+    private Button b1, b2, b3, b4, b5, tod;
     private SeekBar sB;
     private MediaPlayer vectormp[] = new MediaPlayer[11];
     private Handler myHandler = new Handler();
     private double starTime = 0;
     private double finalTime = 0;
-    String him[] = {"ANGELES BLANCOS", "ALLA EN EL CIELO", "CONSEJO DIVINO", "CUANTO DOLOR", "DIVINO COMPAÑERO", "LUZ DE LA MAÑANA", "MUY PRONTO VENDRA", "REGRESA","UN DIA DE BODAS", "JUVENTUD", "YO SOLO ESPERO"};
-    int repetir = 0, posicion = 0,contp=0,conta=0,a=0,sound;
+    String him[] = {"ANGELES BLANCOS", "ALLA EN EL CIELO", "CONSEJO DIVINO", "CUANTO DOLOR", "DIVINO COMPAÑERO", "LUZ DE LA MAÑANA", "MUY PRONTO VENDRA", "REGRESA", "UN DIA DE BODAS", "JUVENTUD", "YO SOLO ESPERO"};
+    int repetir = 0, posicion = 0, contp = 0, conta = 0, a = 0, sound;
     public static int oneTimeOnly = 0;
 
     private final static String TAG = "MediaPlayer audios";
     private MediaPlayer mediaPlayer;
-    private int[] sounds = {R.raw.angelesblancos, R.raw.allaenelcielo, R.raw.consejodivino,R.raw.cuantodolor,R.raw.divinocompanero,R.raw.luzdelamanana,R.raw.muyprontovendra,R.raw.regresa,R.raw.undiadebodas,R.raw.juventud,R.raw.yosoloespero};
+    private int[] sounds = {R.raw.angelesblancos, R.raw.allaenelcielo, R.raw.consejodivino, R.raw.cuantodolor, R.raw.divinocompanero, R.raw.luzdelamanana, R.raw.muyprontovendra, R.raw.regresa, R.raw.undiadebodas, R.raw.juventud, R.raw.yosoloespero};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,22 +64,22 @@ public class Reproductor extends AppCompatActivity implements MediaPlayer.OnComp
         b4.setBackgroundResource(R.drawable.ant);
         b5.setBackgroundResource(R.drawable.sig);
 
-        mediaPlayer  = mediaPlayer.create(this, sounds[0]);
+        mediaPlayer = mediaPlayer.create(this, sounds[0]);
         mediaPlayer.setOnCompletionListener(this);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (conta==0){
+                if (conta == 0) {
                     if (vectormp[posicion].isPlaying()) {
                         vectormp[posicion].pause();
                         b1.setBackgroundResource(R.drawable.play);
-                        contp=0;
+                        contp = 0;
                         Toast.makeText(getApplication(), "PAUSA", Toast.LENGTH_SHORT).show();
-                    }else {
+                    } else {
                         vectormp[posicion].start();
                         b1.setBackgroundResource(R.drawable.pausa);
-                        contp=1;
+                        contp = 1;
                         Toast.makeText(getApplication(), "PLAY", Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -172,34 +172,34 @@ public class Reproductor extends AppCompatActivity implements MediaPlayer.OnComp
         tod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (contp==0){
-                    if (sounds.length > a){
-                        if (mediaPlayer.isPlaying()){
+                if (contp == 0) {
+                    if (sounds.length > a) {
+                        if (mediaPlayer.isPlaying()) {
                             mediaPlayer.stop();
-                            conta=0;
-                            Toast.makeText(getApplication(),"Stop ",Toast.LENGTH_SHORT).show();
-                        }else {
+                            conta = 0;
+                            Toast.makeText(getApplication(), "Stop ", Toast.LENGTH_SHORT).show();
+                        } else {
                             mediaPlayer.start();
                             tex1.setText(him[posicion]);
-                            conta=1;
-                            Toast.makeText(getApplication(),"Se reproducira todo",Toast.LENGTH_SHORT).show();
+                            conta = 1;
+                            Toast.makeText(getApplication(), "Se reproducira todo", Toast.LENGTH_SHORT).show();
                             a++;
                         }
                     } else {
-                        Toast.makeText(getApplication(),"Fin",Toast.LENGTH_SHORT).show();
-                        conta=0;
+                        Toast.makeText(getApplication(), "Fin", Toast.LENGTH_SHORT).show();
+                        conta = 0;
                         mediaPlayer.stop();
                     }
-                 }else {
-                    Toast.makeText(getApplication(),"Play",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplication(), "Play", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private void play(){
+    private void play() {
         sound++;
-        if (sounds.length <= sound){
+        if (sounds.length <= sound) {
             //Termina reproducción de todos los audios.
             return;
         }
@@ -211,21 +211,20 @@ public class Reproductor extends AppCompatActivity implements MediaPlayer.OnComp
             mediaPlayer.start();
             tex1.setText(him[sound]);
             afd.close();
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             Log.e(TAG, "IllegalArgumentException Unable to play audio : " + e.getMessage());
-        }
-        catch (IllegalStateException e) {
+        } catch (IllegalStateException e) {
             Log.e(TAG, "IllegalStateException Unable to play audio : " + e.getMessage());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.e(TAG, "IOException Unable to play audio : " + e.getMessage());
         }
     }
+
     @Override
     public void onCompletion(MediaPlayer mp) {
         play();
     }
+
     //codigo detener musica al pulsar regresar
     public void onBackPressed() {
         mediaPlayer.stop();
